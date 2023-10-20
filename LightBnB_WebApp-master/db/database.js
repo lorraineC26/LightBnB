89e75b02d3e9f67cbc5a94bc1d0134b9b86d5a10
lorraineC26 <lorraine.quinyee@gmail.com>
2023-10-19 23:38:08 -0400

    make addProperty to insert into Db via pool query

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -179,11 +179,24 @@ const getAllProperties = function(options, limit) {
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
-const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+const addProperty = function(property) {
+
+  const queryStr = `
+    INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+    RETURNING *
+  `;
+
+  const values = [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms, property.country, property.street, property.city, property.province, property.post_code];
+
+  return pool
+    .query(queryStr, values)
+    .then((result) => {
+      return result.rows[0];
+    })
+    .catch(err => {
+      console.log(err.message);
+    });
 };
 
 module.exports = {
